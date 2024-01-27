719d3da697d0abd711524069e526cbb2f7e245f5
Pavlen <pavlen@192.168.1.33>
2024-01-27 17:13:29 +0300

    Валидация данных с Pydantic-параметры пути & параметры запроса

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import requests
+
 
 app = FastAPI()
 
 # Стучимся к приложению app
-@app.get("/hotels")
-def get_hotels():
-    return "Отель Бридж Резорт 5 звезд"
+@app.get("/hotels/{hotel_id}")
+def get_hotels(hotel_id: int, date_from, date_to):
+    """hotel_id - параметр пути(path).
+    date_from и date_to - параметры запроса.(query)."""
+    return hotel_id, date_from, date_to
+
+
+r = requests.get(
+    "http://127.0.0.1:8000/hotels/12",
+    params={"date_from": "today", "date_to": "tomorrow",}
+)
