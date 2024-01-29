c364d2900ab0a45a843c239ca39e5f21c8674020
Pavlen <pavlen@192.168.1.33>
2024-01-29 22:46:50 +0300

    Валидация данных с Pydantic-post запрос & схема для запроса.

--- app/main.py
+++ app/main.py
@@ -1,18 +1,33 @@
-from fastapi import FastAPI
-import requests
+from fastapi import FastAPI, Query
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 # Стучимся к приложению app
 @app.get("/hotels/{hotel_id}")
-def get_hotels(hotel_id: int, date_from, date_to):
-    """hotel_id - параметр пути(path).
-    date_from и date_to - параметры запроса.(query)."""
-    return hotel_id, date_from, date_to
+def get_hotels(
+    location: str,
+    date_from: date, 
+    date_to: date,
+    has_spa: Optional[bool] = None,
+    stars: Optional[int] = Query(None, ge=1, le=5)
+    ):
+    return date_from, date_to
 
 
-r = requests.get(
-    "http://127.0.0.1:8000/hotels/12",
-    params={"date_from": "today", "date_to": "tomorrow",}
-)
+class SBooking(BaseModel):
+    """Схема данных для передачи post-запроса."""
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+
+# post-запрос
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    """add_booking имеет формат схемы SBooking."""
+    pass
