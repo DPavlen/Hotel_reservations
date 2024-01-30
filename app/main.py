7e61e516aafb8b551e22683a526a70d25085291c
Pavlen <pavlen@192.168.1.33>
2024-01-30 19:03:50 +0300

    Валидация для фронта & response ответ & Вынесение аргументов для get-запроса

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
@@ -6,16 +6,45 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Стучимся к приложению app
-@app.get("/hotels/{hotel_id}")
+
+class SHotel(BaseModel):
+    """Схема данных для отелей. Валидация параметров."""
+    address: str
+    name: str
+    stars: int 
+
+class HotelsSearchArgs:
+    """Вынесенеие аргументов get-запроса в отдельную сущность.
+    Классическая инициализация параметров через магический __init__."""
+    def __init__(
+            self,
+            location: str,
+            date_from: date, 
+            date_to: date,
+            has_spa: bool = Query(default=None),
+            stars: Optional[int] = Query(None, ge=1, le=5),    
+    ) -> None:
+            self.location = location,
+            self.date_from = date_from,
+            self.date_to = date_to,
+            self.has_spa = has_spa,
+            self.stars = stars,
+
+
+# Где response_model=list[SHotel] пример ответа [{}]
+@app.get("/hotels")
 def get_hotels(
-    location: str,
-    date_from: date, 
-    date_to: date,
-    has_spa: Optional[bool] = None,
-    stars: Optional[int] = Query(None, ge=1, le=5)
-    ):
-    return date_from, date_to
+    search_args: HotelsSearchArgs = Depends()
+) -> list[SHotel]:
+    hotels = [
+        {
+            "address": "ул. Гагарина, 1, Алтай",
+            "name": "Super_hotel",
+            "stars": 5
+        }
+    ]
+    """Опциональные параметры None для has_spa, stars."""
+    return search_args
 
 
 class SBooking(BaseModel):
