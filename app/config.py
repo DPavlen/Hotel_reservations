c4421fd9e5a938e3fa157449a8b343f4ff203a27
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2024-09-05 20:16:40 +0300

    1.5. Create DB_NAME = "booking_db" & migrations alembic. Add test_data_db.sql with PGAdmin4.

--- app/config.py
+++ app/config.py
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
 
-
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
--- app/database.py
+++ app/database.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
-
 engine = create_engine(settings.DATABASE_URL)
 # Сессии, они же транзакции в БД
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 
 class Base(DeclarativeBase):
     """Аккумулирует все модели для ревизий алембика."""
